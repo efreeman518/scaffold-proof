f4c187ea849e8bf6ddd987b61aeb4bc4a19b8e47
EbenFreeman <eben.freeman@ayahealthcare.com>
2026-09-12 23:03:53 -0400

    Align Uno release package contract

--- tests/Test.Architecture/UnoReleasePackageTests.cs
+++ tests/Test.Architecture/UnoReleasePackageTests.cs
@@ -5,14 +5,11 @@ namespace Test.Architecture;
 public sealed class UnoReleasePackageTests
 {
     [TestMethod]
-    public void Given_UnoReleaseBuild_When_ProjectFileRead_Then_RemovesDevelopmentServerAfterImplicitResolution()
+    public void Given_UnoReleaseBuild_When_ProjectFileRead_Then_KeepsOptimizationForSdkAssetExclusion()
     {
         var project = File.ReadAllText(RepoFiles.Path("src", "UI", "TaskFlow.Uno", "TaskFlow.Uno.csproj"));
 
-        StringAssert.Contains(project, "<Target Name=\"_RemoveUnoDevServerFromRelease\"");
-        StringAssert.Contains(project, "AfterTargets=\"UnoImplicitPackages\"");
-        StringAssert.Contains(project, "BeforeTargets=\"CollectPackageReferences\"");
-        StringAssert.Contains(project, "Condition=\"'$(Configuration)' == 'Release'\"");
-        StringAssert.Contains(project, "<PackageReference Remove=\"Uno.WinUI.DevServer\" />");
+        StringAssert.Contains(project, "<Optimize Condition=\"'$(Configuration)'=='Release'\">true</Optimize>");
+        Assert.IsFalse(project.Contains("_RemoveUnoDevServerFromRelease", StringComparison.Ordinal));
     }
 }
